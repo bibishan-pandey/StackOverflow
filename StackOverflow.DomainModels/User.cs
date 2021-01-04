de90dd701a241db38a8b7fd33e18a49d061c1f07
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2021-01-04 13:11:23 +0545

    feat: added User domain model

---
+++ StackOverflow.DomainModels/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StackOverflow.DomainModels
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public bool IsAdmin { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+    }
+}
