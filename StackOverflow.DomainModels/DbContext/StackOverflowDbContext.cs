a720f540ca013dfbde8fd9eadee11d171b489352
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2021-01-04 13:42:33 +0545

    feat: added dbcontext in domain models

---
+++ StackOverflow.DomainModels/DbContext/StackOverflowDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using StackOverflow.DomainModels.Models;
+
+namespace StackOverflow.DomainModels.DbContext
+{
+    public class StackOverflowDbContext : System.Data.Entity.DbContext
+    {
+        public StackOverflowDbContext() : base("StackOverflowDbContext") { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Discussion> Discussions { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Vote> Votes { get; set; }
+        public DbSet<VoteType> VoteTypes { get; set; }
+    }
+}
