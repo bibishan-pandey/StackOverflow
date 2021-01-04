7b031f72fe9b1802fee37328ca2b6fed5ccb576e
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2021-01-04 13:13:22 +0545

    feat: added Category domain model

---
+++ StackOverflow.DomainModels/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StackOverflow.DomainModels
+{
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string CategoryName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
