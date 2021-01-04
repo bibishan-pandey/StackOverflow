7b3222075cf70d95ccd39aabef87dd1ddbc18349
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2021-01-04 18:03:14 +0545

    feat: added login view model

---
+++ StackOverflow.ViewModels/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace StackOverflow.ViewModels.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [DisplayName("Email Address")]
+        [RegularExpression(@"(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})",
+            ErrorMessage = "Email address is not valid!")]
+        public string Email { get; set; }
+
+        [Required]
+        [DisplayName("Password")]
+        public string Password { get; set; }
+    }
+}
