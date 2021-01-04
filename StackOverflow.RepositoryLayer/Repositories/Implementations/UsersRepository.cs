ef7815b2822561eaf56df36fcb20002b15cf6c93
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2021-01-04 16:22:03 +0545

    feat: added user repository

---
+++ StackOverflow.RepositoryLayer/Repositories/Implementations/UsersRepository.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using StackOverflow.DomainModels.DbContext;
+using StackOverflow.DomainModels.Models;
+using StackOverflow.RepositoryLayer.Repositories.Interfaces;
+
+namespace StackOverflow.RepositoryLayer.Repositories.Implementations
+{
+    public class UsersRepository : IUsersRepository
+    {
+        private readonly StackOverflowDbContext _dbContext;
+
+        public UsersRepository(StackOverflowDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IQueryable<User> GetList()
+        {
+            return _dbContext.Users
+                .Where(user => !user.IsAdmin)
+                .OrderByDescending(user => user.CreatedAt);
+        }
+
+        public User GetById(int id)
+        {
+            return _dbContext.Users.Find(id);
+        }
+
+        public void Insert(User model)
+        {
+            _dbContext.Users.Add(model);
+        }
+
+        public void Update(User model)
+        {
+            _dbContext.Entry(model).State = EntityState.Modified;
+        }
+
+        public void UpdatePassword(User model)
+        {
+            
+        }
+
+        public User GetByEmail(string email)
+        {
+            return _dbContext.Users
+                .SingleOrDefault(user => !user.IsAdmin && user.Email == email);
+        }
+
+        public User GetByEmailPassword(string email, string password)
+        {
+            return _dbContext.Users
+                .SingleOrDefault(user => !user.IsAdmin 
+                                        && user.Email == email
+                                        && user.Password == password);
+        }
+
+        public int GetRecentUser()
+        {
+            return _dbContext.Users
+                .Select(user => user.Id)
+                .Max();
+        }
+
+        public void Delete(int? id)
+        {
+            var user = _dbContext.Users.Find(id);
+            _dbContext.Users.Remove(user ?? throw new InvalidOperationException());
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+    }
+}
---
+++ StackOverflow.RepositoryLayer/Repositories/Interfaces/IUsersRepository.cs
@@ -0,0 +1,13 @@
+﻿using StackOverflow.DomainModels.Models;
+using StackOverflow.RepositoryLayer.Repositories.Generics;
+
+namespace StackOverflow.RepositoryLayer.Repositories.Interfaces
+{
+    public interface IUsersRepository : IGenericRepository<User>
+    {
+        void UpdatePassword(User model);
+        User GetByEmail(string email);
+        User GetByEmailPassword(string email, string password);
+        int GetRecentUser();
+    }
+}
