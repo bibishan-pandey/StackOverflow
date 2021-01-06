d82a62eebbefb9fbf81dcdfccac7517eb148ceab
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2021-01-06 12:56:09 +0545

    feat: category view model added

---
+++ StackOverflow.ViewModels/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace StackOverflow.ViewModels.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Category Name")]
+        public string CategoryName { get; set; }
+    }
+}
