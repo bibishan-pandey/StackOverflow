d67a4d8fd52e8ccdba5dfecb2b96256c065f8731
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2021-01-06 14:02:29 +0545

    feat: automapper extension ignore unmapped properties for view models and domain models added

---
+++ StackOverflow.ServiceLayers/Extensions/MapperExtensions.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+
+namespace StackOverflow.ServiceLayers.Extensions
+{
+    public static class MapperExtensions
+    {
+        private static void IgnoreUnmappedProperties(TypeMap typeMap, IMappingExpression mappingExpression)
+        {
+            foreach (var propertyName in typeMap.GetUnmappedPropertyNames())
+            {
+                if (typeMap.SourceType.GetProperty(propertyName) != null)
+                {
+                    mappingExpression.ForMember(propertyName, memberOptions => memberOptions.Ignore());
+                }
+                if (typeMap.DestinationType.GetProperty(propertyName) != null)
+                {
+                    mappingExpression.ForMember(propertyName, memberOptions => memberOptions.Ignore());
+                }
+            }
+        }
+
+        public static void IgnoreUnmapped(this IProfileExpression profileExpression)
+        {
+            profileExpression.ForAllMaps(IgnoreUnmappedProperties);
+        }
+    }
+}
