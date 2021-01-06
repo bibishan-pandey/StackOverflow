a1c349db9a61262f94112d5282a8221dc672199a
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2021-01-06 13:14:51 +0545

    feat: new comment view model added

---
+++ StackOverflow.ViewModels/ViewModels/NewCommentViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace StackOverflow.ViewModels.ViewModels
+{
+    public class NewCommentViewModel
+    {
+        [Required]
+        [DisplayName("Comment")]
+        public string CommentText { get; set; }
+
+        [Required]
+        [DisplayName("User")]
+        public int UserId { get; set; }
+
+        [Required]
+        [DisplayName("Discussion")]
+        public int DiscussionId { get; set; }
+    }
+}
