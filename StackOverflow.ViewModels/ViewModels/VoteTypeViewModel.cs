f4098381f1901accc1604dcda42612d4feb01848
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2021-01-06 13:41:17 +0545

    feat: votetype view model added

---
+++ StackOverflow.ViewModels/ViewModels/VoteTypeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StackOverflow.ViewModels.ViewModels
+{
+    public class VoteTypeViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Vote { get; set; }
+    }
+}
---
+++ StackOverflow.ViewModels/ViewModels/VoteViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace StackOverflow.ViewModels.ViewModels
+{
+    public class VoteViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Vote Type")]
+        public int VoteTypeId { get; set; }
+
+        [Required]
+        [DisplayName("User")]
+        public int UserId { get; set; }
+
+        [Required]
+        [DisplayName("Discussion")]
+        public int DiscussionId { get; set; }
+
+        [DisplayName("Comment")]
+        public int? CommentId { get; set; }
+
+        public virtual VoteTypeViewModel VoteType { get; set; }
+        public virtual UserViewModel User { get; set; }
+        public virtual DiscussionViewModel Discussion { get; set; }
+        public virtual CommentViewModel Comment { get; set; }
+    }
+}
