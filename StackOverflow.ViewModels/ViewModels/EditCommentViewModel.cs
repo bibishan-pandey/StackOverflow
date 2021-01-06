5398783dbc9a99d75a0de765bb125f4e1e13cf0f
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2021-01-06 13:35:26 +0545

    feat: edit comment view model added

---
+++ StackOverflow.ViewModels/ViewModels/EditCommentViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace StackOverflow.ViewModels.ViewModels
+{
+    public class EditCommentViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Comment")]
+        public string CommentText { get; set; }
+        
+        [Required]
+        [DisplayName("User")]
+        public int UserId { get; set; }
+
+        [Required]
+        [DisplayName("Discussion")]
+        public int DiscussionId { get; set; }
+
+        public DiscussionViewModel Discussion { get; set; }
+    }
+}
