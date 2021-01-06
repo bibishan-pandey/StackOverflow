26cc0c7b73f0c53a097e7759d0bd5bc7e172d25f
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2021-01-06 14:17:10 +0545

    feat: sha256 hash generator helper class added in service layer

---
+++ StackOverflow.ServiceLayers/Helpers/Sha256HashGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace StackOverflow.ServiceLayers.Helpers
+{
+    public class Sha256HashGenerator
+    {
+        public static string GenerateHash(string toBeHashedText)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(toBeHashedText));
+                var stringBuilder = new StringBuilder();
+                foreach (var b in bytes)
+                    stringBuilder.Append(b.ToString("x2"));
+
+                return stringBuilder.ToString();
+            }
+        }
+    }
+}
