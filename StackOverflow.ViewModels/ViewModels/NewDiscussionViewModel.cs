b0d4a24289cba1b46aec28b242a643148e78185f
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2021-01-06 13:00:21 +0545

    feat: new discussion view model added

---
+++ StackOverflow.ViewModels/ViewModels/NewDiscussionViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace StackOverflow.ViewModels.ViewModels
+{
+    public class NewDiscussionViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Discussion Title")]
+        public string DiscussionTitle { get; set; }
+
+        [Required]
+        [DisplayName("Discussion Text")]
+        public string DiscussionText { get; set; }
+        
+        [Required]
+        public int Views { get; set; }
+        
+        public int UserId { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
